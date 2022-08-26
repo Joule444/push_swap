6d6b81d529655e09d959f2b23cee7c3ce2b3a1bb
Jules VM <julesthuys@gmail.com>
2022-08-26 16:39:41 +0200

    good need opti

--- bubble_sort.c
+++ bubble_sort.c
@@ -6,7 +6,7 @@
 /*   By: jules <jules@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 15:31:05 by jules             #+#    #+#             */
-/*   Updated: 2022/08/23 18:03:06 by jules            ###   ########.fr       */
+/*   Updated: 2022/08/26 15:36:20 by jules            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,49 +24,8 @@ void	empty_b(t_list **stack_a, t_list **stack_b)
 	}
 }
 
-void	grab_start(t_list **stack_a, t_list **stack_b, t_list *bubble_a, t_list *bubble_b)
+void	sort_elem(t_list **stack_a, t_list **stack_b, t_list *end_a, t_list *end_b)
 {
-	t_list	*elem_a;
-	t_list	*elem_b;
-
-	elem_a = *stack_a;
-	elem_b = *stack_b;
-	while (elem_a->content >= bubble_a->content && elem_b->content <= bubble_b->content)
-	{
-		rotate_r(stack_a, stack_b);
-		elem_a = *stack_a;
-		elem_b = *stack_b;
-	}
-	while (elem_a->content >= bubble_a->content)
-	{
-		rotate_a(stack_a);
-		elem_a = *stack_a;
-	}
-	while (elem_b->content < bubble_b->content)
-	{
-		rotate_b(stack_b);
-		elem_b = *stack_b;
-	}
-}
-
-void	sort_elem(t_list **stack, t_list *end)
-{
-	t_list	*elem;
-
-	elem = *stack;
-	while (elem != end)
-	{
-		if (elem->content > elem->next->content)
-			swap_a(stack);
-		rotate_a(stack);
-		elem = *stack;
-	}
-}
-
-void	bubble_sort(t_list **stack_a, t_list **stack_b, t_list *end_a, t_list *end_b)
-{
-	t_list	*bubble_a;
-	t_list	*bubble_b;
 	t_list	*elem_a;
 	t_list	*elem_b;
 
@@ -84,17 +43,30 @@ void	bubble_sort(t_list **stack_a, t_list **stack_b, t_list *end_a, t_list *end_
 		elem_a = *stack_a;
 		elem_b = *stack_b;
 	}
-	sort_elem(stack_a, end_a);
-	sort_elem(stack_b, end_b);
-	
-	bubble_a = *stack_a;
-	bubble_b = *stack_b;
-	rev_rotate_r(stack_a, stack_b);
-	rev_rotate_r(stack_a, stack_b);
+	while (elem_a != end_a)
+	{
+		if (elem_a->content > elem_a->next->content)
+			swap_a(stack_a);
+		rotate_a(stack_a);
+		elem_a = *stack_a;
+	}
+	while (elem_b != end_b)
+	{
+		if (elem_b->content < elem_b->next->content)
+			swap_b(stack_b);
+		rotate_b(stack_b);
+		elem_b = *stack_b;
+	}
+}
 
+void	sort_elem_back(t_list **stack_a, t_list **stack_b, t_list *bubble_a, t_list *bubble_b)
+{
+	t_list	*elem_a;
+	t_list	*elem_b;
+	
 	elem_a = *stack_a;
 	elem_b = *stack_b;
-	while (elem_a != bubble_a && elem_b != bubble_b)
+	while (elem_a != bubble_a && elem_b != bubble_b && elem_a->content <= bubble_a->content && elem_b->content >= bubble_b->content)
 	{
 		if (elem_a->content > elem_a->next->content && elem_b->content < elem_b->next->content)
 			swap_s(stack_a, stack_b);
@@ -106,25 +78,36 @@ void	bubble_sort(t_list **stack_a, t_list **stack_b, t_list *end_a, t_list *end_
 		elem_a = *stack_a;
 		elem_b = *stack_b;
 	}
-	while (elem_a != bubble_a)
+	while (elem_a != bubble_a && elem_a->content <= bubble_a->content)
 	{
 		if (elem_a->content > elem_a->next->content)
 			swap_a(stack_a);
-		rotate_a(stack_a);
+		rev_rotate_a(stack_a);
 		elem_a = *stack_a;
 	}
-	while (elem_b != bubble_b)
+	while (elem_b != bubble_b && elem_b->content >= bubble_b->content)
 	{
 		if (elem_b->content < elem_b->next->content)
 			swap_b(stack_b);
-		rotate_b(stack_b);
+		rev_rotate_b(stack_b);
 		elem_b = *stack_b;
 	}
+}
 
+void	bubble_sort(t_list **stack_a, t_list **stack_b, t_list *end_a, t_list *end_b)
+{
+	t_list	*bubble_a;
+	t_list	*bubble_b;
+	
+	sort_elem(stack_a, stack_b, end_a, end_b);
+	bubble_a = *stack_a;
+	bubble_b = *stack_b;
+	rev_rotate_r(stack_a, stack_b);
+	rev_rotate_r(stack_a, stack_b);
+	sort_elem_back(stack_a, stack_b, bubble_a, bubble_b);
 	rotate_r(stack_a, stack_b);
-	// grab_start(stack_a, stack_b, bubble_a, bubble_b);
-	// if (!check_sorted(stack_a, *stack_a, ft_lstlast(*stack_a)) || !check_sorted_rev(stack_b, *stack_b, ft_lstlast(*stack_b)))
-	// 	bubble_sort(stack_a, stack_b, lst_previous(stack_a, bubble_a), lst_previous(stack_b, bubble_b));
+	if (!check_sorted(stack_a, *stack_a, ft_lstlast(*stack_a)) || !check_sorted_rev(stack_b, *stack_b, ft_lstlast(*stack_b)))
+		bubble_sort(stack_a, stack_b, lst_previous(stack_a, bubble_a), lst_previous(stack_b, bubble_b));
 }
 
 void	sort(t_list **stack_a, t_list **stack_b)
@@ -133,5 +116,5 @@ void	sort(t_list **stack_a, t_list **stack_b)
 		return ;
 	split_stack(stack_a, stack_b, ft_lstlast(*stack_a));
 	bubble_sort(stack_a, stack_b, ft_lstlast(*stack_a), ft_lstlast(*stack_b));
-	// empty_b(stack_a, stack_b);
+	empty_b(stack_a, stack_b);
 }
--- split_stack.c
+++ split_stack.c
@@ -6,7 +6,7 @@
 /*   By: jules <jules@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 20:48:34 by jules             #+#    #+#             */
-/*   Updated: 2022/08/23 17:31:47 by jules            ###   ########.fr       */
+/*   Updated: 2022/08/24 17:01:00 by jules            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ int	get_mid(t_list **stack)
 		del_elem(stack, lower);	
 		i++;
 	}
-	return (free(stack), tab[size / 2]);
+	return (free(stack), tab[(size / 2) - 1]);
 }
 
 void	split_stack(t_list **stack_a, t_list **stack_b, t_list *end)
@@ -85,13 +85,13 @@ void	split_stack(t_list **stack_a, t_list **stack_b, t_list *end)
 	mid = get_mid(stack_copy(stack_a));
 	while (elem != end)
 	{
-		if (elem->content < mid)
+		if (elem->content <= mid)
 			push_b(stack_a, stack_b);
 		else
 			rotate_a(stack_a);
 		elem = *stack_a;
 	}
-	if (elem->content < mid)
+	if (elem->content <= mid)
 		push_b(stack_a, stack_b);
 	else
 		rotate_a(stack_a);
